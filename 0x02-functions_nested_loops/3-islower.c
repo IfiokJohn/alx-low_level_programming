e5cc97c882c2f4c5ed4ebcdb309eb58b3c62d03b
Ifiok John <ifiokjohnij@gmail.com>
2022-10-23 05:05:51 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _islower "main.h"
+ * _islower - Entry point
  * Description: checks is a character is lowercase
  * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
