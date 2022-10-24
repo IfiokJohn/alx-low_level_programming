7b2978348af8a057841a61b8780c2f67b66ad7ef
Ifiok John <ifiokjohnij@gmail.com>
2022-10-24 09:48:07 -0700

    task 3 -5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,16 @@
 
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int counter = 0;
-	int i;
+		int len = 0, index = 0;
+		char tmp;
+		while (s[index++])
+			len++;
 
-	while (s[counter] != '\0')
-	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		for (index = len - 1; index >= len / 2;
+		index--)
+		{
+			tmp = s[index];
+			s[index] = s[len - index - 1];
+			s[len - index - 1] = tmp;
+		}
 	}
-}
-
