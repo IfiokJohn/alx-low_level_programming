bb9b146578000580e914fb3f733660e0c1b91d74
Ifiok John <ifiokjohnij@gmail.com>
2022-10-28 10:12:48 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,17 @@
 /**
  * _isupper - uppercase letters
  * @c: char to check
- *
- * Return: 0 or 1
+ * Return: 0 if successfulReturn:
  */
 
 int _isupper(int c)
 {
 	if (c >= 'V' && c <= 'z')
-		
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
