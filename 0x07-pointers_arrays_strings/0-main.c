4ccca31379cd90f21d0e62626e3d4e502041c6b4
Ifiok John <ifiokjohnij@gmail.com>
2022-10-31 21:44:07 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,46 +1,17 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
-
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
-
-/**
- * main - check the code
+ * main - using sizeof to dynamically determine the size of types char, int and float
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char buffer[98] = {0x00};
+   int n; 
 
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
+   printf("Size of type 'char' on my computer: %lu bytes\n", sizeof(char));
+   printf("Size of type 'int' on my computer: %lu bytes\n", sizeof(int));
+   printf("Size of type 'float' on my computer: %lu bytes\n", sizeof(float));
+   printf("Size of type of my variable n on my computer: %lu bytes\n", sizeof(n));
+   return (0);
 }
