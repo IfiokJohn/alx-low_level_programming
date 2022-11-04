f41bc0c8b0e08a7c9b171bf500fa676b84c8bbc1
Ifiok John <ifiokjohnij@gmail.com>
2022-11-04 00:45:13 -0700

    task 1 - adv 8

---
+++ 0x08-recursion/_recursion.c
@@ -0,0 +1,23 @@
+
+t@3a83df1c5e5a:~/alx-low_level_programming/0x08-recursion# vi 2-strlen_recursion.c
+root@3a83df1c5e5a:~/alx-low_level_programming/0x08-recursion# vi _recursion.c -o 1-print_rev_recursion
+root@3a83df1c5e5a:~/alx-low_level_programming/0x08-recursion# vi 2-strlen_recursion.c
+root@3a83df1c5e5a:~/alx-low_level_programming/0x08-recursion# vi 2-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
+}
+
+
